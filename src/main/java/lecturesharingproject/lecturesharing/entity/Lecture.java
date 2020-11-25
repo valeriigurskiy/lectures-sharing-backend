f81e633cf428e4514aafb44bb14f5c3112ffe27b
freefireman <qwrtui132>
2020-11-25 17:13:37 +0200

    entity lecture complete

--- src/main/java/lecturesharingproject/lecturesharing/entity/Lecture.java
+++ src/main/java/lecturesharingproject/lecturesharing/entity/Lecture.java
@@ -4,16 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Entity
 public class Lecture {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(nullable = false, length = 16)
     private LessonName lessonName;
+    @Column(nullable = false, length = 32)
     private String title;
+    @Column(nullable = false, length = 64)
     private String lessonDescription;
+}
 
 
