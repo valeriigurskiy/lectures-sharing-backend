d45c68b45d8701fa6f5fd32d5c4f82ee068c698d
freefireman <qwrtui132>
2020-11-25 16:02:25 +0200

    Initial commit

---
+++ src/main/java/lecturesharingproject/lecturesharing/LecturesharingApplication.java
@@ -0,0 +1,13 @@
+package lecturesharingproject.lecturesharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LecturesharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LecturesharingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lecturesharingproject/lecturesharing/LecturesharingApplicationTests.java
@@ -0,0 +1,13 @@
+package lecturesharingproject.lecturesharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LecturesharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
