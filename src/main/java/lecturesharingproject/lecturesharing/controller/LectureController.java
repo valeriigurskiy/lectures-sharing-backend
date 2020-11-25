37f0a954c3900bb3e5c48734bfa0e975b54a0e6f
sviatiq <sviatiq@gmail.com>
2020-11-25 17:55:09 +0200

    added probably fix error of bean creating

--- src/main/java/lecturesharingproject/lecturesharing/controller/LectureController.java
+++ src/main/java/lecturesharingproject/lecturesharing/controller/LectureController.java
@@ -2,39 +2,21 @@ package lecturesharingproject.lecturesharing.controller;
 
 import lecturesharingproject.lecturesharing.entity.Lecture;
 import lecturesharingproject.lecturesharing.service.LectureService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/lectures")
-@Slf4j
 public class LectureController {
 
-    @Autowired
     private LectureService lectureService;
-
-    @GetMapping
-    public List<Lecture> getLectures() {
-        return lectureService.getAllLectures();
+    @Autowired
+    public LectureController(LectureService lectureService){
+        this.lectureService = lectureService;
     }
 
-    @GetMapping(value = "/{id}")
-    public Lecture findLectureById(@PathVariable int id){
-        return lectureService.getLecture(id);
-    }
-
-    @PostMapping
-    public Lecture insertLecture(@RequestBody Lecture lecture) {
-        Lecture newLecture = new Lecture(lecture.getId(), lecture.getLessonName(), lecture.getLessonDescription(), lecture.getTitle());
-        return lectureService.insertLecture(newLecture);
-    }
-
-    @DeleteMapping(value = "/{id}")
-    public void deleteLecture(@PathVariable int id) {
-        lectureService.removeLecture(id);
+    @GetMapping(value = "/lectures")
+    public List<Lecture> getLectures(){
+        return this.lectureService.getLectures();
     }
 }
