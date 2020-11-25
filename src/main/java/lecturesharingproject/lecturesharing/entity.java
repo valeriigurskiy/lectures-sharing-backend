d45c49b0c97578c77912c4c91f590244cf3a85b6
sviatiq <sviatiq@gmail.com>
2020-11-25 16:53:21 +0200

    Last commit

--- src/main/java/lecturesharingproject/lecturesharing/entity.java
+++
@@ -1,6 +0,0 @@
-package lecturesharingproject.lecturesharing;
-
-public class entity {
-    int id;
-    //sa;sla;sl
-}
--- src/main/java/lecturesharingproject/lecturesharing/entity/Lecture.java
+++ src/main/java/lecturesharingproject/lecturesharing/entity/Lecture.java
@@ -5,15 +5,19 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Entity
 public class Lecture {
+    @Id
     private int id;
     private LessonName lessonName;
     private String title;
     private String lessonDescription;
+}
+
 
 
