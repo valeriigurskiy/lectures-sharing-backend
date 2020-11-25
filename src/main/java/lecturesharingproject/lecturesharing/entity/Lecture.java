d616424ae37db7cb4e64164df12e7d8b2cc80192
sviatiq <sviatiq@gmail.com>
2020-11-25 16:15:14 +0200

    #2 Added constructor and get/set methods

--- src/main/java/lecturesharingproject/lecturesharing/entity/Lecture.java
+++ src/main/java/lecturesharingproject/lecturesharing/entity/Lecture.java
@@ -6,4 +6,43 @@ public class Lecture {
     private String title;
     private String lessonDescription;
 
+    public Lecture(int id, LessonName lessonName, String title, String lessonDescription) {
+        this.id = id;
+        this.lessonName = lessonName;
+        this.title = title;
+        this.lessonDescription = lessonDescription;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LessonName getLessonName() {
+        return lessonName;
+    }
+
+    public void setLessonName(LessonName lessonName) {
+        this.lessonName = lessonName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLessonDescription() {
+        return lessonDescription;
+    }
+
+    public void setLessonDescription(String lessonDescription) {
+        this.lessonDescription = lessonDescription;
+    }
 }
