08cb8f27c4aa7d9f4a26af4a5954be60c652c9f4
sviatiq <sviatiq@gmail.com>
2020-11-25 17:47:07 +0200

    some commit

--- src/main/java/lecturesharingproject/lecturesharing/entity/Lecture.java
+++ src/main/java/lecturesharingproject/lecturesharing/entity/Lecture.java
@@ -5,12 +5,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Entity
 public class Lecture {
+    @Id
     private int id;
     private LessonName lessonName;
     private String title;
