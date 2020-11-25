a2226b3621813b1f82794a8e223f4124eed6edf0
sviatiq <sviatiq@gmail.com>
2020-11-25 16:11:39 +0200

    #1 Added Lecture entity

---
+++ src/main/java/lecturesharingproject/lecturesharing/entity/Lecture.java
@@ -0,0 +1,9 @@
+package lecturesharingproject.lecturesharing.entity;
+
+public class Lecture {
+    private int id;
+    private LessonName lessonName;
+    private String title;
+    private String lessonDescription;
+
+}
