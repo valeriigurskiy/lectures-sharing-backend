4069785dbac1749ff7ee4e011feb27ea7b526d7c
sviatiq <sviatiq@gmail.com>
2020-11-25 16:53:21 +0200

    Last commit

--- src/main/java/lecturesharingproject/lecturesharing/entity/Lecture.java
+++ src/main/java/lecturesharingproject/lecturesharing/entity/Lecture.java
@@ -4,7 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -12,14 +13,11 @@ import javax.persistence.*;
 @Entity
 public class Lecture {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @Column(nullable = false, length = 16)
     private LessonName lessonName;
-    @Column(nullable = false, length = 32)
     private String title;
-    @Column(nullable = false, length = 64)
     private String lessonDescription;
 }
+
 
 
